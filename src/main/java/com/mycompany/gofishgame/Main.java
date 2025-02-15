7aafe92b610249c1c7226afb04861ef2c0b07df0
ekambains <bainseka@sheridancollege.ca>
2025-02-14 20:31:23 -0500

    Finalized GoFish

--- src/main/java/com/mycompany/gofishgame/Main.java
+++ src/main/java/com/mycompany/gofishgame/Main.java
@@ -3,13 +3,25 @@
  */
 package com.mycompany.gofishgame;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        List<Player> players = Arrays.asList(new Player("Somya"), new Player("Chann"));
+        Scanner scanner = new Scanner(System.in);
+        List<Player> players = new ArrayList<>();
+
+        for (int i = 0; i < 2; i++) {
+            System.out.print("Enter Player Name: " + (i + 1) + ": ");
+            String playerName = scanner.nextLine();
+            players.add(new Player(playerName));
+        }
+
         GoFishGame game = new GoFishGame("Go Fish", players);
         game.play();
+
+        scanner.close();
     }
 }
